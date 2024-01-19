9b01158dbeba814d01828606115d481824accd07
KubaKrzych <krzychowiec.k@gmail.com>
2024-01-19 22:29:38 +0100

    Modify settings

--- notes/settings.py
+++ notes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^^nwqe0z7$6sfwbgmw)^+(qj&vmfon*a%0&#^u-%n&*lwg35a="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "notes.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "localhost",
+        "NAME": "db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "PORT": "5432",
     }
 }
 
@@ -116,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = Path.joinpath(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DJANGO_SETTINGS_MODULE = "notes.settings"
